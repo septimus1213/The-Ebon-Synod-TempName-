b47fad6b5bcf989d0e36e9971ea58d57f92f7cbd
Krisztián Kiss <110058838+septimus1213@users.noreply.github.com>
2025-10-09 21:49:59 +0200

    first commit bullshit

---
+++ scr_player/player.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+@export var speed = 300.0
+
+func _physics_process(delta):
+	
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direction * speed
+	move_and_slide()
