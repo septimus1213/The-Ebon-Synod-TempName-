03abeac0eb154ed64b91d3c8f5b63b84a8ae1abe
Slex736 <amtjkersten@gmail.com>
2025-10-10 14:13:18 +0200

    player smaller background added and player can dash

--- scr_player/player.gd
+++ scr_player/player.gd
@@ -1,9 +1,15 @@
 extends CharacterBody2D
 
 @export var speed = 300.0
+@export var dashspeed = 5000
 
 func _physics_process(delta):
 	
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * speed
+	
+	var dash = Input.is_action_just_pressed("dash")
+	if dash and direction != Vector2.ZERO:
+		velocity = direction * dashspeed
+		
 	move_and_slide()
