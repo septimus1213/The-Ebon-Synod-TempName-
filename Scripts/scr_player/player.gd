9ab01ec1ca18dc84a56f8883159978fe0154b2f2
Slex736 <amtjkersten@gmail.com>
2025-10-12 19:45:44 +0200

    player walk animations

--- Scripts/scr_player/player.gd
+++ Scripts/scr_player/player.gd
@@ -5,6 +5,9 @@ extends CharacterBody2D
 @export var dashspeed = 3000
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var timer: Timer = $Timer
+@onready var animated_sprite_2d: AnimatedSprite2D = $Walking
+
+
 var is_dash_ready = true
 
 
@@ -12,12 +15,23 @@ var is_dash_ready = true
 # set timer length
 func _ready() -> void:
 	timer.wait_time = DashTimer
+	print(animated_sprite_2d)
 
 # walk  left right up down and dash with a timer between dash of x secconds
 func _physics_process(_delta):
 	
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * speed
+	if direction.x < 0:
+		animated_sprite_2d.play("WalkLeft")
+	elif direction.x > 0:
+		animated_sprite_2d.play("WalkRight")
+	elif direction.y < 0:
+		animated_sprite_2d.play("WalkUp")
+	elif direction.y > 0:
+		animated_sprite_2d.play("WalkDown")
+	else:
+		animated_sprite_2d.play("idle")
 	
 	
 	var dash = Input.is_action_just_pressed("dash")
